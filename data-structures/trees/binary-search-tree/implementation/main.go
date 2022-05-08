d9dc50d6226b1330b9c068da7acc909a55d93aa7
Çağrı Kılıçoğlu <mcagrikilicoglu@gmail.com>
2022-05-08 17:24:04 +0300

    Add dynamic programming practice

--- data-structures/trees/binary-search-tree/implementation/main.go
+++ data-structures/trees/binary-search-tree/implementation/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type BinarySearchTree struct {
 	root *Node
@@ -50,18 +52,31 @@ func main() {
 	myBinarySearchTree.insert(15)
 	myBinarySearchTree.insert(1)
 
-	fmt.Println(*myBinarySearchTree)
-	fmt.Println(*myBinarySearchTree.root)
-	fmt.Println(*myBinarySearchTree.root.right)
-	fmt.Println(*myBinarySearchTree.root.left)
-	fmt.Println(*myBinarySearchTree.lookup(4).right)
-	fmt.Println(*myBinarySearchTree.lookup(4).left)
-	fmt.Println(myBinarySearchTree.lookup(20).right)
-	fmt.Println(myBinarySearchTree.lookup(20).left)
-	fmt.Println(myBinarySearchTree.lookup(6))
-	fmt.Println(myBinarySearchTree.lookup(170))
-	fmt.Println(myBinarySearchTree.lookup(15))
-	fmt.Println(myBinarySearchTree.lookup(1))
+	// BFS := myBinarySearchTree.breadthFirstSearch()
+	// fmt.Println(BFS)
+	// BFSR := myBinarySearchTree.breadthFirstSearchRecursive([]int{}, []*Node{myBinarySearchTree.root})
+	// fmt.Println(BFSR)
+
+	DFSInOrder := myBinarySearchTree.DFSInOrder()
+	fmt.Println(DFSInOrder)
+
+	// DFSPreOrder := myBinarySearchTree.DFSPreOrder()
+	// fmt.Println(DFSPreOrder)
+
+	// DFSPostOrder := myBinarySearchTree.DFSPostOrder()
+	// fmt.Println(DFSPostOrder)
+	// fmt.Println(*myBinarySearchTree)
+	// fmt.Println(*myBinarySearchTree.root)
+	// fmt.Println(*myBinarySearchTree.root.right)
+	// fmt.Println(*myBinarySearchTree.root.left)
+	// fmt.Println(*myBinarySearchTree.lookup(4).right)
+	// fmt.Println(*myBinarySearchTree.lookup(4).left)
+	// fmt.Println(myBinarySearchTree.lookup(20).right)
+	// fmt.Println(myBinarySearchTree.lookup(20).left)
+	// fmt.Println(myBinarySearchTree.lookup(6))
+	// fmt.Println(myBinarySearchTree.lookup(170))
+	// fmt.Println(myBinarySearchTree.lookup(15))
+	// fmt.Println(myBinarySearchTree.lookup(1))
 
 	// fmt.Println(preOrderTraverse(*myBinarySearchTree.root))
 
@@ -153,10 +168,120 @@ func (b *BinarySearchTree) remove(value int) {
 
 }
 
-// func preOrderTraverse(node Node) []int {
-// 	values = append(values, node.value)
-// 	if node.left != nil {
-// 		values = append(values, node.left.preOrderTraverse())
-// 	}
-// 	return values
+func (b *BinarySearchTree) breadthFirstSearch() []int {
+	currentNode := b.root
+	list := []int{}
+	queue := []*Node{}
+	queue = append(queue, currentNode)
+
+	for len(queue) > 0 {
+		currentNode = queue[0]
+		queue = queue[1:]
+		list = append(list, currentNode.value)
+
+		if currentNode.left != nil {
+			queue = append(queue, currentNode.left)
+		}
+		if currentNode.right != nil {
+			queue = append(queue, currentNode.right)
+		}
+	}
+	return list
+}
+
+func (b *BinarySearchTree) breadthFirstSearchRecursive(list []int, queue []*Node) []int {
+
+	if len(queue) == 0 {
+		return list
+	}
+	currentNode := queue[0]
+	queue = queue[1:]
+	list = append(list, currentNode.value)
+	if currentNode.left != nil {
+		queue = append(queue, currentNode.left)
+	}
+	if currentNode.right != nil {
+		queue = append(queue, currentNode.right)
+	}
+	return b.breadthFirstSearchRecursive(list, queue)
+}
+
+// func (b *BinarySearchTree) DFSInOrder() []int {
+// 	return traverseInOrder(b.root, []int{})
 // }
+
+// func traverseInOrder(node *Node, list []int) []int {
+// 	log.Println(node.value)
+
+// 	if node.left != nil {
+// 		list = append(list, traverseInOrder(node.left, list)...)
+
+// 	}
+// 	list = append(list, node.value)
+// 	if node.right != nil {
+// 		list = append(list, traverseInOrder(node.right, list)...)
+
+// 	}
+// 	// list = append(list, node.value)
+// 	return list
+// }
+
+func (b *BinarySearchTree) DFSInOrder() []int {
+	return b.root.traverseInOrder()
+}
+
+func (node *Node) traverseInOrder() []int {
+
+	var list []int
+	if node.left != nil {
+		fmt.Println(list)
+		list = append(list, node.left.traverseInOrder()...)
+
+	}
+	list = append(list, node.value)
+
+	if node.right != nil {
+		list = append(list, node.right.traverseInOrder()...)
+
+	}
+
+	return list
+}
+
+func (b *BinarySearchTree) DFSPreOrder() []int {
+	return b.root.traversePreOrder()
+}
+
+func (node *Node) traversePreOrder() []int {
+
+	var list []int
+	list = append(list, node.value)
+	if node.left != nil {
+		list = append(list, node.left.traversePreOrder()...)
+	}
+
+	if node.right != nil {
+		list = append(list, node.right.traversePreOrder()...)
+	}
+
+	return list
+}
+
+func (b *BinarySearchTree) DFSPostOrder() []int {
+	return b.root.traversePostOrder()
+}
+
+func (node *Node) traversePostOrder() []int {
+
+	var list []int
+
+	if node.left != nil {
+		list = append(list, node.left.traversePostOrder()...)
+	}
+
+	if node.right != nil {
+		list = append(list, node.right.traversePostOrder()...)
+	}
+	list = append(list, node.value)
+	return list
+}
